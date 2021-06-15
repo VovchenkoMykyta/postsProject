e356ab0f0d8e12cdb21636be8eabb9a78331541b
greezlu <greezlu@gmail.com>
2021-06-16 00:55:44 +0300

    Add future functions

--- vendor/MySQL/PostDatabase.php
+++ vendor/MySQL/PostDatabase.php
@@ -1,13 +1,15 @@
 <?php
-  
+
 namespace MySQL;
-  
+
 final class PostDatabase extends Database {
-  
-  static private $tableName = "posts";
-  
-    static public function addPost () {}
-    static public function removePost () {}
-    static public function editPost () {}
-  
+
+    static private $tableName = "posts";
+
+    static public function addPost(string $name, string $content, $creationDate, $authorId) {}
+    static public function removePost(int $id, int $emitterId) {}
+    static public function editPost(int $id, string $name, string $content) {}
+    static public function getPost(int $id) {}
+    static public function getPostPage (int $offset, int $maxPosts) {}
+
 }
