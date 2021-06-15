491b6de3c452180faf0b7f3e39dde06b9bed18c2
Инна <85671934+Innefer@users.noreply.github.com>
2021-06-15 21:03:22 +0300

    Database

--- vendor/MySQL/PostDatabase.php
+++ vendor/MySQL/PostDatabase.php
@@ -4,6 +4,8 @@ namespace MySQL;
   
 final class PostDatabase extends Database {
   
+  static private $tableName = "posts";
+  
     static public function addPost () {}
     static public function removePost () {}
     static public function editPost () {}
