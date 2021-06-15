610435267ab273c1faf93c09355d90260d22aad6
Инна <85671934+Innefer@users.noreply.github.com>
2021-06-15 20:52:33 +0300

    insert

--- vendor/MySQL/Database.php
+++ vendor/MySQL/Database.php
@@ -7,7 +7,7 @@ class Database {
   static private $db;
   static protected $connectionStatus;
   
-  static protected function connect () {
+  static private function connect () {
   
       if (self::$db && self::$connectionStatus) return;
   
@@ -18,10 +18,38 @@ class Database {
       } else {
         self::$connectionStatus = true;
       }
-      
+
   }
   
-  static protected function insert () {}
+  static public function insert ( string $tableName, array $values) {
+    
+    if (!self::$db) self::connect();
+    if (!self::$connectionStatus) return false;
+    
+    $query = "INSERT INTO `$tableName` ";
+
+    $fieldList = array_keys($values);
+    
+    $query .= "(";
+    
+    foreach ($fieldList as $field) {
+      $query .= "`$field`, ";
+    }
+    
+    $query = substr($query, 0, -2).") ";
+    
+    $query .= "VALUES (";
+    
+    foreach ($values as $value) {
+      $query .= "'".addslashes($value)."', ";
+    }
+    
+    $query = substr($query, 0, -2).");";
+    
+    return self::$db->query($query);
+    
+  }
+  
   static protected function update () {}
   static protected function delete () {}
   static protected function select () {}
