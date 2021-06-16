eaccd2ef7324606f6b2203de9512d131fba2ac1c
VovchenkoMykyta <vovchenkonikita36@gmail.com>
2021-06-16 18:46:55 +0300

    modified Router.php

--- vendor/Base/Router.php
+++ vendor/Base/Router.php
@@ -2,4 +2,48 @@
 
 namespace Base;
 
-final class Router {}
+use MySQL\UserDatabase;
+
+final class Router {
+
+    static public function init(){
+        $userDB = new UserDatabase();
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+            $user = filter_input_array(INPUT_POST);
+
+            $userDB->addUser($user['login'], $user['password'], $user['repeat']);
+
+            self::redirect();
+
+        }else if($_SERVER['REQUEST_METHOD'] === 'GET' ){
+
+            if(isset($_GET['id'])){
+                $deleteId = filter_input(INPUT_GET, 'id');
+                $emitterId = $_COOKIE['id'];
+
+                if($deleteId!==null){
+
+                    $id = intval($deleteId);
+
+                    $userDB->removeUser($id, $emitterId);
+
+                }
+                self::redirect();
+            }
+
+
+        }else{
+
+            $userDB->getUserList();
+
+        }
+
+    }
+
+    static public function redirect(){
+
+        header('Location: index.php');
+
+    }
+}
