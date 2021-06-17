e55be6f381bf7fa61a4167dd6d1a7e2fc11c9b4d
Инна <85671934+Innefer@users.noreply.github.com>
2021-06-17 20:11:58 +0300

    Add news view

--- view/admin-news-add.php
+++ view/admin-news-add.php
@@ -1,6 +1,5 @@
-<form action="/admin/user/add" method="POST">
-    <input type="text" name="login">
-    <input type="password" name="password">
-    <input type="password" name="repeat">
-    <input type="submit">
+<form action="/admin/news/add" method="POST">
+    <input type="text" name="name">
+    <input type="textarea" name="content">
+    <input type="submit" value="ADD POST">
 </form>
