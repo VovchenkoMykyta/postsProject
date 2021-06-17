0d28cd60cc02747cb0b09e65ef535e5bc91bc950
greezlu <greezlu@gmail.com>
2021-06-17 13:10:58 +0300

    Empty view files

---
+++ view/user-template.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>News</title>
+</head>
+<body>
+<header>
+    Logo
+</header>
+<main>
+    <?php include_once $this->pageFile; ?>
+</main>
+<footer>
+    postsProject &copy;
+</footer>
+</body>
+</html>
