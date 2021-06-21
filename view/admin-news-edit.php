7e67afc1d5a75a40dea13740d6d57e473b539e86
VovchenkoMykyta <vovchenkonikita36@gmail.com>
2021-06-21 14:51:09 +0300

    fix

--- view/admin-news-edit.php
+++ view/admin-news-edit.php
@@ -2,7 +2,7 @@
 <?php if ($post) : ?>
 <form action="/admin/news/edit" method="POST">
     <input type="text" name="name" value="<?= $post["name"] ?>">
-    <textarea><?= $post["content"] ?></textarea>
+    <textarea name="content"><?= $post["content"] ?></textarea>
     <input type="hidden" name="id" value="<?= $post["id"] ?>">
     <input type="submit" value="EDIT POST">
 </form>
