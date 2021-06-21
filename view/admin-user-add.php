d0b08a3d04c7512253ab39fd74fe0a721d714b0b
VovchenkoMykyta <vovchenkonikita36@gmail.com>
2021-06-21 14:50:00 +0300

    modified files, add styles

--- view/admin-user-add.php
+++ view/admin-user-add.php
@@ -1,6 +1,6 @@
 <form action="/admin/user/add" method="POST">
-    <input type="text" name="login">
-    <input type="password" name="password">
-    <input type="password" name="repeat">
+    <input type="text" name="login" placeholder="Enter login">
+    <input type="password" name="password" placeholder="Enter password">
+    <input type="password" name="repeat" placeholder="Confirm password">
     <input type="submit" value="Sign Up">
 </form>
