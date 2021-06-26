52fd1b8e83163255d3924b3119347007e2afd6c3
VovchenkoMykyta <vovchenkonikita36@gmail.com>
2021-06-26 11:43:14 +0300

    form and js fix

--- js/index.js
+++ js/index.js
@@ -1,31 +1,77 @@
-function check() {
+function validateLogin() {
 
-    let login = document.getElementById("login").value;
-    let pwd = document.getElementById("pass").value;
+    let login = document.getElementById('login').value;
 
-    let pwd_expression = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])/;
-    let letters = /^[A-Za-z]+$/;
+    if(login.length === 0) {
 
-    if (login === '') {
-        msg('invalid', 'msg-login', 'red');
-        if(pwd === ''){
-            msg( 'invalid', 'msg-pass', 'red');
-        }
-    } else if (!letters.test(login)) {
-        msg( 'invalid', 'msg-pass', 'red');
-    } else if (!pwd_expression.test(pwd)) {
-        msg( 'invalid', 'msg-pass', 'red');
-    } else if (document.getElementById("pass").value.length < 6) {
-        msg( 'small password', 'msg-pass', 'red');
-    } else {
-        msg('valid', 'msg-login', 'green');
-        msg('valid', 'msg-pass', 'green');
+        producePrompt('invalid', 'msg-login' , 'red');
+        return false;
+
+    }
+
+    if (!login.match(/^[A-Za-z]+$/)) {
+
+        producePrompt('invalid','msg-login', 'red');
+        return false;
+
+    }
+
+    producePrompt('Valid', 'msg-login', 'green');
+    return true;
+
+}
+
+function validatePass() {
+
+    let pass = document.getElementById('pass').value;
+
+    if(pass.length === 0) {
+        producePrompt('invalid', 'msg-pass', 'red');
+        return false;
+    }
+
+    if(pass.length < 6) {
+        producePrompt('invalid', 'msg-pass', 'red');
+        return false;
+    }
+
+    if(!pass.match(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])/)) {
+        producePrompt('invalid' ,'msg-pass', 'red');
+        return false;
+    }
+
+    producePrompt('Valid', 'msg-pass', 'green');
+    return true;
+
+}
+
+function validateForm() {
+    if (!validateLogin() || !validatePass()) {
+        jsShow('msg-login');
+        jsShow('msg-pass');
+        producePrompt('invalid', 'msg-login', 'red');
+        producePrompt('invalid', 'msg-pass', 'red');
+        setTimeout(function(){jsHide('msg-login');}, 3000);
+        setTimeout(function(){jsHide('msg-pass');}, 3000);
+        return false;
+    }
+    else {
+
     }
 }
 
-function msg(message, location, color) {
+function jsShow(id) {
+    document.getElementById(id).style.display = 'inline-block';
+}
 
-    document.getElementById(location).innerHTML = message;
-    document.getElementById(location).style.color = color;
+function jsHide(id) {
+    document.getElementById(id).style.display = 'none';
+}
+
+
+function producePrompt(message, promptLocation, color) {
+
+    document.getElementById(promptLocation).innerHTML = message;
+    document.getElementById(promptLocation).style.color = color;
 
 }
--- view/admin-login.php
+++ view/admin-login.php
@@ -1,9 +1,9 @@
-<form action="/login" method="POST" onsubmit="check()">
+<form action="/login" method="POST" onsubmit="validateForm()">
     <label>Login:
-        <input type="text" name="login" id="login"><span id="msg-login"></span>
+        <input type="text" name="login" id="login" onkeyup="validateLogin()"><span id="msg-login"></span>
     </label>
     <label>Password:
-        <input type="password" name="password" id="pass"><span id="msg-pass"></span>
+        <input type="password" name="password" id="pass" onkeyup="validatePass()"><span id="msg-pass"></span>
     </label>
     <input type="submit" value="Login" id="sub">
 </form>
