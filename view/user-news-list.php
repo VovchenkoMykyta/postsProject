cdbb9d8b8b959aa40fe998f8ce55e50eca197752
greezlu <greezlu@gmail.com>
2021-06-29 15:47:43 +0300

    Fix link to one post

--- view/user-news-list.php
+++ view/user-news-list.php
@@ -1,11 +1,11 @@
 <div id="news-list">
     <?php
-    $posts = \MySQL\PostDatabase::getPostPage(0, 5);
+    $postsList = \MySQL\PostDatabase::getPostPage(0, 5);
 
-    foreach ($posts as $post1){
-        $id = $post1['id'];
-        echo "<div><a href='view/user-news-one.php'>".$post1['name']."</a></div>";
-        echo "<a href='view/user-news-one.php'><div>". \MySQL\PostDatabase::getSmallContent($post1['content'])."</div></a>";
+    foreach ($postsList as $post){
+        $id = $post['id'];
+        echo "<div><a href='/news/?id=$id'>".$post['name']."</a></div>";
+        echo "<a href='/news/?id=$id'><div>". \MySQL\PostDatabase::getSmallContent($post['content'])."</div></a>";
     }
     ?>
 </div>
