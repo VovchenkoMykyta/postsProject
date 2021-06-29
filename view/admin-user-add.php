10b1179935783248102942725630f1b40d3ed771
greezlu <greezlu@gmail.com>
2021-06-30 02:00:38 +0300

    User add page

--- view/admin-user-add.php
+++ view/admin-user-add.php
@@ -1,6 +1,6 @@
 <form action="/admin/user/add" method="POST">
-    <input type="text" name="login" placeholder="Enter login">
-    <input type="password" name="password" placeholder="Enter password">
-    <input type="password" name="repeat" placeholder="Confirm password">
+    <input type="text" name="login" placeholder="Login">
+    <input type="password" name="password" placeholder="Password">
+    <input type="password" name="repeat" placeholder="Repeat">
     <input type="submit" value="Sign Up">
 </form>
