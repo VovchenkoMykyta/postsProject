f65d757621f42dc9e8dde684d68d3c078b6fe6da
greezlu <greezlu@gmail.com>
2021-06-30 01:47:55 +0300

    Edit admin template

--- view/admin-template.php
+++ view/admin-template.php
@@ -1,11 +1,13 @@
 <?php
 
+    $userId = NULL;
+    $userLogin = NULL;
+
     if (isset($_SESSION["user_id"]) && $_SESSION["login"] === "yes") {
         $userId = $_SESSION["user_id"];
-    } else {
-        $userId = NULL;
+        $userLogin = \MySQL\UserDatabase::getUserById($userId)["login"];
     }
-    
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -17,32 +19,45 @@
 </head>
 <body>
 <header>
-    <h1>Admin Panel</h1>
-    <ul id="links">
-        <li><a href="/admin/news/list">News List</a></li>
-        <li><a href="/admin/news/add">News Add</a></li>
-        <li><a href="/admin/user/list">Users List</a></li>
-        <li><a href="/admin/user/add">User Add</a></li>
-    </ul>
+
+    <div id="logo">
+        <h1>Admin Panel</h1>
+    </div>
+
     <?php if ( !is_null($userId) ) : ?>
-        <h2>User: <?= \MySQL\UserDatabase::getUserById($userId)["login"] ?> </h2>
-        <form action="/logout" method="POST"><input type="submit" value="logout"></form>
+        <div id="user">
+            <span><?= $userLogin ?></span>
+            <form action="/logout" method="POST"><input type="submit" value="logout"></form>
+        </div>
     <?php endif;?>
+    
 </header>
 <main>
     
-    <?php
-        if ( isset($_SESSION["errors"]) ) {
-            var_dump($_SESSION["errors"]);
-            unset($_SESSION["errors"]);
-        }
-    ?>
-        
-    <?php include_once "./view/".$this->pageFile.".php"; ?>
-    
+    <?php if ( !is_null($userId) ) : ?>
+        <nav id="left-side-bar">
+            <ul id="links">
+            <li><a href="/admin/news/list">News List</a></li>
+            <li><a href="/admin/news/add">News Add</a></li>
+            <li><a href="/admin/user/list">Users List</a></li>
+            <li><a href="/admin/user/add">User Add</a></li>
+            </ul>
+        </nav>
+    <?php endif;?>
+
+    <div id="container">
+        <?php
+            if ( isset($_SESSION["errors"]) ) {
+                var_dump($_SESSION["errors"]);
+                unset($_SESSION["errors"]);
+            }
+        ?>
+        <?php include_once "./view/".$this->pageFile.".php"; ?>
+    </div>
+   
 </main>
 <footer>
-    postsProject &copy;
+    <a href="https://github.com/VovchenkoMykyta/postsProject" id="copyright">postsProject &copy;</a>
 </footer>
 <script src="../js/index.js"></script>
 </body>
