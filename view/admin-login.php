da6887c2327e64586c091b5acd7c294c74fa3d3a
greezlu <greezlu@gmail.com>
2021-06-30 09:46:07 +0300

    admin-login

--- view/admin-login.php
+++ view/admin-login.php
@@ -1,9 +1,5 @@
-<form action="/login" method="POST" onsubmit="validateForm()">
-    <label>Login:
-        <input type="text" name="login" id="login" onkeyup="validateLogin()"><span id="msg-login"></span>
-    </label>
-    <label>Password:
-        <input type="password" name="password" id="pass" onkeyup="validatePass()"><span id="msg-pass"></span>
-    </label>
-    <input type="submit" value="Login" id="sub">
+<form action="/login" method="POST">
+    <input type="text" name="login" id="login" placeholder="Login"><span id="msg-login"></span>
+    <input type="password" name="password" id="pass" placeholder="Password"><span id="msg-pass"></span>
+    <input type="submit" value="Login">
 </form>
