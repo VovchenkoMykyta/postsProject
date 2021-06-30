67f06f0d938ddb2ecaaaf30ca2f914732f7d3f1e
greezlu <greezlu@gmail.com>
2021-07-01 00:34:50 +0300

    user-template

--- view/user-template.php
+++ view/user-template.php
@@ -7,14 +7,24 @@
     <title>News</title>
 </head>
 <body>
-<header>
-    <h1>D-NEWS</h1>
-</header>
-<main>
-    <?php include_once "./view/".$this->pageFile.".php"; ?>
-</main>
-<footer>
-    postsProject &copy;
-</footer>
+
+    <header>
+        <h1>D-NEWS</h1>
+    </header>
+
+    <main>
+        <?php
+            if ( isset($_SESSION["errors"]) ) {
+                var_dump($_SESSION["errors"]);
+                unset($_SESSION["errors"]);
+            }
+        ?>
+        <?php include_once "./view/".$this->pageFile.".php"; ?>
+    </main>
+
+    <footer>
+        <a href="https://github.com/VovchenkoMykyta/postsProject" id="copyright">postsProject &copy;</a>
+    </footer>
+
 </body>
 </html>
