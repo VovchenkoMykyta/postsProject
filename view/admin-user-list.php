ca8d79c4ebdf1522f9cc95fd52133abc4f0a19af
greezlu <greezlu@gmail.com>
2021-06-30 16:21:45 +0300

    admin-user-list

--- view/admin-user-list.php
+++ view/admin-user-list.php
@@ -1,38 +1,33 @@
-<?php
-    $users = MySQL\UserDatabase::getUserList();
-?>
-
+<?php $users = MySQL\UserDatabase::getUserList(); ?>
 <?php if ($users) : ?>
-<div id="container-user-table">
-<table id="admin-user-table">
+<div id="table-wrapper">
+    <table>
 
-    <thead>
-        <tr>
-            <?php foreach (array_keys($users[0]) as $key): ?>
-            <td><?= $key ?></td>
-            <?php endforeach ?>
-            <td>Action</td>
-        </tr>
-    </thead>
-    
-    <tbody>
-    <?php foreach ($users as $post): ?>
-        <tr>
-            <?php foreach ($post as $field): ?>
-            <td><?= $field ?></td>
-            <?php endforeach ?>
-            <td>
-                <form action="/admin/user/remove/" method="POST">
-                    <input type="hidden" value="<?= $post["id"] ?>" name="id">
-                    <input type="submit" value="Delete">
-                </form>
-            </td>
-        </tr>
-    <?php endforeach ?>
-    </tbody>
+        <thead>
+            <tr>
+                <td class="user-id">User ID</td>
+                <td class="user-login">Login</td>
+                <td class="user-action">Action</td>
+            </tr>
+        </thead>
+        
+        <tbody>
+        <?php foreach ($users as $user): ?>
+            <tr>
+                <td class="user-id"><?= $user["id"] ?></td>
+                <td class="user-login"><?= $user["login"] ?></td>
+                <td class="user-action">
+                    <form action="/admin/news/remove/" method="POST">
+                        <input type="hidden" value="<?= $user["id"] ?>" name="id">
+                        <input type="submit" value="Delete">
+                    </form>
+                </td>
+            </tr>
+        <?php endforeach ?>
+        </tbody>
 
-    <tfoot></tfoot>
+        <tfoot></tfoot>
 
-</table>
+    </table>
 </div>
 <?php endif ?>
