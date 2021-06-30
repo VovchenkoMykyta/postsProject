8642936d0370ac33c00cea82fb29a9a80a7933ce
greezlu <greezlu@gmail.com>
2021-06-30 16:22:16 +0300

    admin-news-edit

--- view/admin-news-edit.php
+++ view/admin-news-edit.php
@@ -1,9 +1,12 @@
 <?php $post = \MySQL\PostDatabase::getPostById($this->data["id"]) ?>
+
 <?php if ($post) : ?>
+
 <form action="/admin/news/edit" method="POST">
     <input type="text" name="name" value="<?= $post["name"] ?>">
     <textarea name="content"><?= $post["content"] ?></textarea>
     <input type="hidden" name="id" value="<?= $post["id"] ?>">
-    <input type="submit" value="EDIT POST">
+    <input type="submit" value="Edit post">
 </form>
+
 <?php endif ?>
