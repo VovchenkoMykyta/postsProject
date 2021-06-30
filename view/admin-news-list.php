26d568c09e15188757c3a07f2d37d8ffe7fe4def
greezlu <greezlu@gmail.com>
2021-06-30 12:09:36 +0300

    admin-news-list

--- view/admin-news-list.php
+++ view/admin-news-list.php
@@ -3,35 +3,40 @@
 ?>
 
 <?php if ($posts) : ?>
-<table>
+<div id="table-wrapper">
+    <table>
 
-    <thead>
-        <tr>
-            <?php foreach (array_keys($posts[0]) as $key): ?>
-            <td><?= $key ?></td>
-            <?php endforeach ?>
-            <td>Action</td>
-        </tr>
-    </thead>
-    
-    <tbody>
-    <?php foreach ($posts as $post): ?>
-        <tr>
-            <?php foreach ($post as $field): ?>
-            <td><?= $field ?></td>
-            <?php endforeach ?>
-            <td>
-                <a href="/admin/news/edit/?id=<?= $post["id"] ?>">Edit</a>
-                <form action="/admin/news/remove/" method="POST">
-                    <input type="hidden" value="<?= $post["id"] ?>" name="id">
-                    <input type="submit" value="Delete">
-                </form>
-            </td>
-        </tr>
-    <?php endforeach ?>
-    </tbody>
+        <thead>
+            <tr>
+                <td class="post-id">Post ID</td>
+                <td class="author">Author</td>
+                <td class="post-name">Post Name</td>
+                <td class="post-date">Creation date</td>
+                <td class="post-action">Action</td>
+            </tr>
+        </thead>
+        
+        <tbody>
+        <?php foreach ($posts as $post): ?>
+            <?php $userLogin = \MySQL\UserDatabase::getUserById($post["user_id"])["login"] ?? "ID: {$post["user_id"]}"; ?>
+            <tr>
+                <td class="post-id"><?= $post["id"] ?></td>
+                <td class="author"><?= $userLogin ?></td>
+                <td class="post-name"><?= $post["name"] ?></td>
+                <td class="post-date"><?= $post["creation_date"] ?></td>
+                <td class="post-action">
+                    <a href="/admin/news/edit/?id=<?= $post["id"] ?>">Edit</a>
+                    <form action="/admin/news/remove/" method="POST">
+                        <input type="hidden" value="<?= $post["id"] ?>" name="id">
+                        <input type="submit" value="Delete">
+                    </form>
+                </td>
+            </tr>
+        <?php endforeach ?>
+        </tbody>
 
-    <tfoot></tfoot>
+        <tfoot></tfoot>
 
-</table>
+    </table>
+</div>
 <?php endif ?>
