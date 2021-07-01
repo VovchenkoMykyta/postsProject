c916af70fbb8cdbe82daf3302b98c3272c5f2524
greezlu <greezlu@gmail.com>
2021-07-01 12:06:51 +0300

    user-news-one

--- view/user-news-one.php
+++ view/user-news-one.php
@@ -1,5 +1,29 @@
-<?php $post = \MySQL\PostDatabase::getPostById($this->data["id"]) ?>
+<?php
+    $post = \MySQL\PostDatabase::getPostById($this->data["id"]);
+    $userLogin = \MySQL\UserDatabase::getUserById($post["user_id"])["login"] ?? "ID: {$post["user_id"]}";
+?>
 
-<p>PostName:<?= $post["name"] ?></p>
+<div id="news-item">
 
-<p>PostName:<?= $post["content"] ?></p>
+    <div class="caption">
+    <?= $post["name"] ?>
+    </div>
+
+    <div class="content">
+        <span><?= $post["content"] ?></span>
+    </div>
+
+    <div class="credentials">
+
+        <div class="author">
+            <span><?= $userLogin ?></span>
+        </div>
+
+        <div class="date">
+            <span><?= $post["creation_date"] ?></span>
+        </div>
+
+    </div>
+
+</div>
+
